5b8069cd7d0d75c86f10be3723f0d5dbaf560082
Chris <c.lamothe25@gmail.com>
2018-06-10 17:44:11 -0400

    Initial commit...... exercises 1 and 2

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#print("Hello World!")
+#print("Hello again")
+#print("This is fun.")
+#print('Yay, Printing!')
+#print("I'd much rather you 'not'.")
+#print("I like typing this.")
+#print('I "said" do not touch this.')
+print("I love my dogs.")
