78162bdefad760137bb70b328238aabaac40fc29
Chris <c.lamothe25@gmail.com>
2018-06-10 19:54:41 -0400

    Math and numbers.... exercise 3

---
+++ ex3.py
@@ -0,0 +1,31 @@
+print("I will now count my chickens:")
+
+#addition, division
+print("Hens", 25 + 30 / 6)
+#subtraction, multiplication,modulus
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+#addition, subtraction, modulus, division
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#numbers are part of the string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#addition, less than, subtraction
+print(3 + 2 < 5 - 7)
+
+#addition
+print("What is 3 + 2?", 3 + 2)
+#subtraction
+print("What is 5 - 7", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+#next 3 are comparison operators
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= 2)
+print("Is it less or equal?", 5 <= 2)
