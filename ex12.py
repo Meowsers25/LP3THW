492f32ff421b3bffc7ca4439d9674359d1ffa98b
Chris <c.lamothe25@gmail.com>
2018-06-15 07:36:38 -0400

    Uses more input and prompts user

---
+++ ex12.py
@@ -0,0 +1,8 @@
+# we will rewrite the last exercise.......You can put a string in the input to #
+# prompt user
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} years old, {height} tall and you weigh {weight} pounds.")
