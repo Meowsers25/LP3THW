70c585a7917d99915069df420ff1a79b1f83a71c
Chris <c.lamothe25@gmail.com>
2018-06-15 07:14:00 -0400

    input() and end=' '

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# end=' ' keeps the cursor on the same line --- no newline escape
+# input() takes user input and stores it in the provided variable
