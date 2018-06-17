f570f61508d4721b70375147ee28d0f122fd2ea9
Chris <c.lamothe25@gmail.com>
2018-06-16 20:00:36 -0400

    Uses argv ... user gives input on the command line

---
+++ ex13.py
@@ -0,0 +1,10 @@
+# script is another name for Python files
+
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("The first variable is:", first)
+print("The second variable is:", second)
+print("The third variable is:", third)
